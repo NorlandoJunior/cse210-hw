fbfcf697586bba3238a579e11b3fd3009ba5ac17
Norlando_jr <norlandojr@hotmail.com>
2025-03-05 14:20:22 -0300

    commit Exercise1 complete

---
+++ week01/Exercise1/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Exercise1
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! This is the Exercise1 Project..");
+
+            Console.WriteLine("What is your first name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            var last_name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {last_name}, {name} {last_name}.");
+        }
+    }
+}
