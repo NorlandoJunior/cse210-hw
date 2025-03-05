baef4c7a6bf8867ee3161e1e432cd6b662e523bd
Norlando_jr <norlandojr@hotmail.com>
2025-03-05 19:03:01 -0300

    commit exercise3 done!

---
+++ week01/Exercise3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program {
+
+public static void Main(string[] args)
+{
+
+
+
+    Random randomGeneretor = new Random();
+    int magicNumber = randomGeneretor.Next(1, 101);
+
+    int guess = -1;
+    
+    // diferent the magic number reapeat/loop
+    
+    while (guess != magicNumber)
+    
+    { 
+
+     Console.WriteLine("What is your guess?");
+     string answer = Console.ReadLine();
+
+     guess = int.Parse(answer);
+    
+     if (magicNumber > guess)
+     {
+        Console.WriteLine("Higer!");
+     }
+     else if (magicNumber < guess)
+     {
+        Console.WriteLine("Lower!");
+     }
+     else
+     {
+        Console.WriteLine("You guessed it!");
+     }
+    }
+}
+}
+
+
