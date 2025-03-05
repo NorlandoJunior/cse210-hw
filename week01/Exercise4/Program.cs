e316ad0f74730f6183a1c65017db2fc20c36843b
Norlando_jr <norlandojr@hotmail.com>
2025-03-05 19:49:03 -0300

    start the exercise4

---
+++ week01/Exercise4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+  public static void Main(string[] args)
+ {
+    Console.writeLine("Enter a list of numbers, type 0 when finished")
+    int 
+
+
+    while ()
+
+ }
+
+
+}
