1182af2757f68ae2b9e779527b9103faf4fca9b5
Norlando_jr <norlandojr@hotmail.com>
2025-03-06 20:19:00 -0300

    aaaa

--- week01/Exercise2/obj/Debug/net9.0/Exercise2.AssemblyInfo.cs
+++ week01/Exercise2/obj/Debug/net9.0/Exercise2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e316ad0f74730f6183a1c65017db2fc20c36843b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1da0866cb9ca8eba0f8739fc2c47a0805feb03d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
