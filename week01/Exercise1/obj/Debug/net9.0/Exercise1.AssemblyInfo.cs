7ebd14cc0fc4858e77f9cad53eed7b189a7d6803
Norlando_jr <norlandojr@hotmail.com>
2025-03-29 08:22:06 -0300

    comit to udpatade code

--- week01/Exercise1/obj/Debug/net9.0/Exercise1.AssemblyInfo.cs
+++ week01/Exercise1/obj/Debug/net9.0/Exercise1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd9191c932b1a1e34e1fe1087a7c3d8c9e9eec9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ebdd4f981cac27bd564c921841fd282568e51f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
